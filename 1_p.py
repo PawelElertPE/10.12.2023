c9fbfdaf195472e474c22e4c19378bd6ffa41615
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-12-10 10:18:08 +0100

    10:15

--- 1_p.py
+++ 1_p.py
@@ -3,7 +3,18 @@ import argparse
 parser = argparse.ArgumentParser()
 
 parser.add_argument('filename')
+parser.add_argument(*name_or_flags: '-m', '--mode')
+
 
 args = parser.parse_args()
 
 print(f'Nazwa pliku to: {args.filename}')
+print(f'Tryb to: {args.mode}')
+
+with open(args.filename, 'r') as file1:
+    content = file1.read()
+
+
+
+
+
---
+++ 2_debugger.py
@@ -0,0 +1,5 @@
+a = 0
+for i in range(10):
+    a += 1
+    print(f'Iteraja {i + 1}')
+    print(f'a = {a}')
